a77b50ad3642f9ea4619aacfc0d89e73f7187d0d
victrfiuza <victorfiuza@icloud.com>
2022-11-17 01:35:57 -0300

    result-n1-n2-is-{}

--- 003.py
+++ 003.py
@@ -1,4 +1,5 @@
 n1 = int(input('please enter a value: '))
 n2 = int(input('please enter another value:'))
 sum = n1+n2
-print('the result is: {}'.format(sum))
+print('the result {} + {} is: {}'.format(n1, n2, sum))
+
