e80abb8bca8c7fc90927bd9ac5c486b854dcf722
victrfiuza <victorfiuza@icloud.com>
2022-11-18 00:01:23 -0300

    type-isspace-isnumeric-isalpha

---
+++ 004-is.py
@@ -0,0 +1,11 @@
+n= input('enter: ')
+print('\ntype:',type(n))
+print(f"space: {n.isspace()} \n\
+number: {n.isnumeric()} \n\
+alpha: {n.isalpha()} \n\
+alnum: {n.isalnum()} \n\
+upper: {n.isupper()} \n\
+lower: {n.islower()} \n\
+title: {n.istitle()}")
+
+#alpha=alfabetico // alnum=alfanumerico // lower=minusculo // uppeer=maiusuculo // lower=minusculo // title:capitalizad
