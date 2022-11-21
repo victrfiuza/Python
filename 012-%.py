4931da909fd65aad003726a8478d6b4e1d65a21b
victrfiuza <victorfiuza@icloud.com>
2022-11-21 20:43:52 -0300

    -5%

--- 012-%.py
+++ 012-%.py
@@ -1,4 +1,4 @@
-price=int(input('price: '))
-pc= 0.05 * price
-price= price-pc
-print(f"new price w 5%: {price} ")
+price = float(input('$'))
+pc = 0.05 * price
+price = price-pc
+print(f"new price w 5%: $ {price:.2f} ")
