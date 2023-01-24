091db7ebdeafa4ec4741465617185ca02105aa01
victrfiuza <victorfiuza@icloud.com>
2023-01-24 16:37:32 -0300

    cos sin tan radians

--- 018.py
+++ 018.py
@@ -1,2 +1,3 @@
-from math import sqrt, floor
-print('')
+from math import cos, sin, tan, radians
+ang = float(input('Digite um numero: '))
+print(f'cos: {cos(radians(ang)):.2f} // tan: {tan(radians(ang)):.2f} // sen:{sin(radians(ang)):.2f}')
