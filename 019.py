596502c62b33a1385c03ae5717a3a139d7432769
victrfiuza <victorfiuza@icloud.com>
2023-01-24 16:37:12 -0300

    choice

---
+++ 019.py
@@ -0,0 +1,7 @@
+import random
+name1 = str(input('Digite um nome:'))
+name2 = str(input('Digite um nome:'))
+name3 = str(input('Digite um nome:'))
+name4 = str(input('Digite um nome:'))
+sorteado = [name1, name2, name3, name4]
+print(f'sorteado: {random.choice(sorteado)}')
