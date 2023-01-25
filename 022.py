44455d520586e54c34835942a55d79c98ecc9800
victrfiuza <victorfiuza@icloud.com>
2023-01-25 13:02:29 -0300

    -

--- 022.py
+++ 022.py
@@ -1,5 +1,9 @@
-s = str(input('name: '))
-print(s.upper())  # A
-print(s.lower())  # a
-print(s.count())  # quantas letras ao todo sem considerar espaço
-print(s.strip())
+name = str(input('name: '))
+print(f'Upper:{name.upper()}')  # A
+print(f'Lower:{name.lower()}')  # a
+fullname = name.replace(' ', '')
+print(f'Full name: {len(fullname)}')
+# foi divido e agora o 0 representa a primeira palavra.
+fname = name.split()
+print(f'First name: {fname[0]}')
+print(f'First name: {len(fname[0])}')
---
+++ 023.py
@@ -0,0 +1,6 @@
+n = str(input('Digite um numero: '))
+
+print(f'Unidade: {n[3]}')
+print(f'Dezena: {n[2]}')
+print(f'Centena: {n[1]}')
+print(f'Milhar: {n[0]}')
