8d24471f83996a079b317aa874898b7e1a28ab8e
victrfiuza <victorfiuza@icloud.com>
2023-01-28 12:18:13 -0300

    if and or

--- 032.py
+++ 032.py
@@ -1,5 +1,5 @@
 ano = int(input('Digite o ano: '))
-if ano % 4 == 0:
+if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print(f'{ano} é um ano bissexto')
 else:
     print(f'{ano} NÃO É UM ANO BISSEXTO')
