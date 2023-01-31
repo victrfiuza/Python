0dd2ea7a15adec235980fad812a2972ec9cad328
victrfiuza <victorfiuza@icloud.com>
2023-01-30 22:52:42 -0300

    peso

---
+++ 054.py
@@ -0,0 +1,12 @@
+peso = 0
+cont = 0
+cont2 = 0
+for i in range(1, 6):
+    peso = float(input('PESO: '))
+    if peso > peso:
+        peso = peso
+    elif peso < peso:
+        peso = peso
+
+print(peso)
+print(peso)
