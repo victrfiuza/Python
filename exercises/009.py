861d9f0b6305d6ec1d8ed2021bc22306d60435f2
victrfiuza <victorfiuza@icloud.com>
2023-03-07 23:55:30 -0300

    rename

---
+++ exercises/009.py
@@ -0,0 +1,2 @@
+n = int(input('enter w the number: '))
+print(f'\n{n} x 1 = {n*1} \n{n} x 2 = {n*2} \n{n} x 3 = {n*3} \n{n} x 4 = {n*4} \n{n} x 5 = {n*5} \n{n} x 6 = {n*6} \n{n} x 7 = {n*7} \n{n} x 8 = {n*8} \n{n} x 9 = {n*9} \n{n} x 10 = {n*10}\n')
